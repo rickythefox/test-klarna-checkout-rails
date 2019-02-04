f398bf9db191dfcf146401ac65db39c934feac53
Richard Ginzburg <1675955+rickythefox@users.noreply.github.com>
2019-02-04 18:33:53 +0000

    Move klarna url to global config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  config.x.klarna.endpoint_base = "https://api.playground.klarna.com"
 end
